0981963318e93644a30bcfe7624ec6b05f41c885
Ralph F <RaftsDev@gmail.com>
2020-10-13 21:17:37 -0400

    Chore: Check.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -54,7 +54,8 @@ public class Main {
         String direction;
         while(true){
             // check immutability
-           // System.out.println("put: "+ tempExits.put("N",2));
+           //s
+            // System.out.println("put: "+ tempExits.put("N",2));
 
             //print current location
             System.out.println("Your are in: "+locations.get(currentRoom).getDescription());
